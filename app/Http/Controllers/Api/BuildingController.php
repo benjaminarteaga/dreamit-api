f8ec4bc410ae55a1a0b325ef1cc3fb867b4a84a7
Benjamín Arteaga <benjaminjarteagag@gmail.com>
2021-01-28 16:56:01 -0300

    Building module

---
+++ app/Http/Controllers/Api/BuildingController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Building;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BuildingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Building::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $building = Building::create($request->all());
+        return response()->json([
+                'message' => 'Edificio registrada exitosamente.',
+                'building' => $building
+            ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Building  $building
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Building $building)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Building  $building
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Building $building)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Building  $building
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Building $building)
+    {
+        $building->update($request->all());
+        return response()->json([
+                'message' => 'Datos del edificio actualizados exitosamente.',
+                'building' => $building
+            ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Building  $building
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Building $building)
+    {
+        $building->delete();
+        return response()->json(['message' => 'Edificio eliminadao.']);
+    }
+}
